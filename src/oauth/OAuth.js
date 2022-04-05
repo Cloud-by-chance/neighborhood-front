9336041d8ec5f59c53d33390844c498fd01f012e
kakjzi <anmern3@naver.com>
2022-04-05 17:56:51 +0900

    FIX

--- src/oauth/OAuth.js
+++ src/oauth/OAuth.js
@@ -1,4 +1,5 @@
-const CLIENT_ID = "b9cde1037a96070da43fd0e36b35b9de";
+import KAKAO_KEY from '../oauth/KAKAO_KEY'
+const CLIENT_ID = "KAKAO_KEY";
 const REDIRECT_URI =  "http://localhost:3000/oauth/callback/kakao";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
