44b37210c688ae2e0b97cd7a546d26f6fe865117
kakjzi <82758364+kakjzi@users.noreply.github.com>
2022-04-06 19:42:50 +0900

    FIX: login api 수정

--- src/oauth/callback/kakao.js
+++ src/oauth/callback/kakao.js
@@ -1,18 +1,28 @@
 // 리다이렉트될 화면
+// 여기서 처리..
 
-import React from "react";
-import { useDispatch } from "react-redux";
-import { actionCreators as userActions } from "../modules/user";
+import axios from "axios";
 
-const Kakao = (props) => {
-  const dispatch = useDispatch();
-
-  // 인가코드
-  let code = new URL(window.location.href).searchParams.get("code");
-  console.log(code);
-  React.useEffect(async () => {
-    dispatch(userActions.kakaoLogin(code));
-  }, []);
+const Kakao = async () => {
+  try {
+    let code = new URL(window.location.href).searchParams.get("code");
+    const result = await axios
+      .get(`http://localhost:8081/oauth/callback/kakako?code=${code}`, {
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+      })
+      .then((res) => {
+        console.log(res.data["access_token"]);
+        // To Do : 토큰 활용로직
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  } catch (error) {
+    console.log("Login error", error);
+    window.alert("Login failed...");
+  }
 };
 
 export default Kakao;
