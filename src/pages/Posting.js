4569cdbf6a771a4028e3a7118b4120e19ba58ce2
havefunatcode <dlxogh439@gmail.com>
2022-04-20 16:28:45 +0900

    게시글 추가 이벤트 처리

--- src/pages/Posting.js
+++ src/pages/Posting.js
@@ -1,6 +1,6 @@
 import Input from "@material-tailwind/react/Input";
-import React, {useState, useRef} from 'react';
-import {Editor} from '@toast-ui/react-editor';
+import React, {useEffect, useState, useRef} from 'react';
+import {Editor, EditorProps} from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import Button from "@material-tailwind/react/Button";
 
@@ -18,42 +18,47 @@ import uml from '@toast-ui/editor-plugin-uml';
 function Post() {
     const editorRef = useRef();
 
+    const [editorCon, setEditorCon] = useState("");
     const [form, setForm] = useState({
         user_id: '',
-        post_name: ''
-        // content: '',
+        post_name: '',
+        content: ''
     });
-
+    // 데이터를 보낼 Form 처리
     const handleChange = (e) => {
-        const {user_id, value} = e.target; // 우선 e.target 에서 user_id와 value 를 추출
-        
-        console.log(value)
-        console.log(user_id)
+        const {name, value} = e.target; // 우선 e.target 에서 user_id와 value 를 추출
 
         setForm({
             ...form,            // 기존의 form 객체 복사
-            [user_id]: value    // user_id 키를 갖는 값을 value로 설정
+            [name]: value,    // name 키를 갖는 값을 value로 설정
+            // content: "hello"
         });
     };
+    // Editor가 변경할 때마다 값을 읽어오게 함.
+    const contentChange = () => {
+        setEditorCon(editorRef.current.getInstance().getHTML());
+    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // onSaveData(form)
-        console.log(form);
+    // 최초 한 번만 실행(Constructor 역할)
+    useEffect(() => {
+        const editorInstance = editorRef.current.getInstance();
+        const getContent_html = editorInstance.getHTML();
         
-        // const editorInstance = editorRef.current.getInstance();
-        // const getContent_md = editorInstance.getMarkdown();        
-        // console.log("---------------Mark Down-----------------");
-        // console.log(getContent_md);
-        // const getContent_html = editorInstance.getHTML();
-        // console.log("---------------Html-----------------");
-        // console.log(getContent_html);
+        setEditorCon(getContent_html);
+    }, [])
 
+    // editorCon값이 변경될 때마다 실행
+    useEffect(() => {
         setForm({
-            post_name: '',
-            user_id: '',
-            // content: '',
+            ...form,
+            content: editorCon
         })
+    }, [editorCon])
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(form);
     }
 
     return (
@@ -66,48 +71,18 @@ function Post() {
                     <Input className="w-24 ml-5" type="text" color="lightBlue" size="regular" outline={true} required placeholder="제목" 
                             name='post_name' value={form.post_name} onChange={handleChange}/>
                 </div>
-                {/* <div className="flex md:flex-row mb-3 h-12 w-3/5 justify-center">
-                    <label htmlFor="username" className="w-full flex-1 mx-2 text-xs font-semibold 
-                    text-gray-600 uppercase">작성자
-                        <input className="w-full py-3 px-1 mt-1 
-                    text-gray-800 appearance-none 
-                    border-2 border-gray-100
-                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
-                            required placeholder='이름을 입력해주세요' type='text' name='user_id' 
-                            value={form.name} onChange={handleChange} />
-                    </label>
-                    <label htmlFor="text" className="w-full flex-1 mx-2 text-xs font-semibold 
-                    text-gray-600 uppercase">제목
-                        <input className="w-full py-3 px-1 mt-1 
-                    text-gray-800 appearance-none 
-                    border-2 border-gray-100
-                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
-                            required placeholder='게시글의 제목을 입력해주세요' type='text' name='post_name' 
-                            value={form.post_name} onChange={handleChange} />
-                    </label> */}
-                    {/* <Textarea class="form-textarea basis-1/4" rows="1"
-                        color="lightBlue" size="regular" outline={true} placeholder="작성자" name='user_id' value={form.user_id} onChange={handleChange}/>
-                    <Textarea
-                        color="lightBlue" size="regular" outline={true} placeholder="제목" name='post_name' value={form.post_name} onChange={handleChange}/> */}
-                {/* </div> */}
                 <div className="mt-5 w-5/6 flex justify-center">
-                    {/* <label htmlFor="content" className="w-full flex-1 mx-2 text-xs font-semibold 
-                    text-gray-600 uppercase">내용
-                        <input className="w-full py-3 px-1 mt-1 
-                    text-gray-800 appearance-none 
-                    border-2 border-gray-100
-                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
-                            required placeholder='내용을 입력해주세요' type='text' name='content'
-                            value={form.content} onChange={handleChange} /> */
-                    /* </label> */}
-                    {/* <Textarea color="lightBlue" size="regular" outline={true} placeholder="내용" name='content' value={form.content} onChange={handleChange}/> */}
                     <Editor initialValue="hello react editor world!"
                             previewStyle="vertical"
                             height="600px"
                             initialEditType="markdown"
                             useCommandShortcut={true}
                             plugins={[chart, codeSyntaxHighlight, colorSyntax, tableMergedCell, uml]}
-                            ref={editorRef} />
+                            ref={editorRef}
+                            name="content"
+                            value={form.content}
+                            onChange={contentChange}
+                            />
                 </div>
                 <div className='text-center'>
                     <Button color="lightBlue" buttonType="outline" size="regular" rounded={false} block={false} iconOnly={false} ripple="dark"
