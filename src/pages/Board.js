944e802b62fbe0f0368149138b5a9adb8ae21b71
havefunatcode <dlxogh439@gmail.com>
2022-04-22 13:02:44 +0900

    Catch Posting page Error

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -77,52 +77,6 @@ function Board() {
           <h1>게 시 판</h1>
         </div>
           { !cookieId ? <></> : <WriteBtn />}
-        <table className="min-w-full table-auto text-gray-800">
-          <thead className="justify-between">
-            <tr className="bg-gray-800">
-              <th className="text-gray-300 px-4 py-3">번호</th>
-              <th className="text-gray-300 px-4 py-3">제목</th>
-              <th className="text-gray-300 px-4 py-3">작성자</th>
-              <th className="text-gray-300 px-4 py-3">최종 수정일</th>
-              <th className="text-gray-300 px-4 py-3">수정</th>
-              <th className="text-gray-300 px-4 py-3">삭제</th>
-            </tr>
-          </thead>
-          <Tr info={info} handleRemove={handleRemove} handleEdit={handleEdit}/>
-        </table>
-
-        <Link
-          to={{
-            pathname: "/post",
-            state: { isEdit: false },
-          }}
-        >
-          <Button
-            className="flex justify-center mb-8 text-xs px-3 py-2"
-            color="lightBlue"
-            buttonType="outline"
-            rounded={false}
-            block={false}
-            iconOnly={false}
-            ripple="dark"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 "
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4v16m8-8H4"
-              />
-            </svg>
-            글 쓰기
-          </Button>
-        </Link>
 
         {/* <label>
           페이지 당 표시할 게시물 수:&nbsp;
