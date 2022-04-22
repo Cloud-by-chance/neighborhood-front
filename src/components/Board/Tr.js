ee1861048c006d2cb4edfa8452b1448bf89cd5ad
kakjzi <anmern3@naver.com>
2022-04-22 11:22:23 +0900

    ADD: 페이징 기능 추가 & 버튼 CSS 수정

--- src/components/Board/Tr.js
+++ src/components/Board/Tr.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import Td from './Td';
+import React from "react";
+import Td from "./Td";
 
-function Tr ({info, handleRemove, handleEdit}) {
-    return (
-        <tbody>
-            {
-                info.map(item => {
-                    return (
-                        <Td key={item.post_id} item={item} handleRemove={handleRemove} handleEdit={handleEdit} />
-                    )
-                })
-            }
-        </tbody>
-    );
-};
+function Tr({ info, handleRemove, handleEdit }) {
+  return (
+    <tbody>
+      {info.map((item) => {
+        return (
+          <Td
+            className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+            key={item.post_id}
+            item={item}
+            handleRemove={handleRemove}
+            handleEdit={handleEdit}
+          />
+        );
+      })}
+    </tbody>
+  );
+}
 
 export default Tr;
--- src/pages/Board.js
+++ src/pages/Board.js
@@ -7,6 +7,7 @@ import Modal from "../components/Board/Modal";
 import Button from "@material-tailwind/react/Button";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { BeakerIcon } from "@heroicons/react/solid";
+import Pagination from "../components/Board/pagination";
 
 function Board() {
   const [info, setInfo] = useState([]);
@@ -14,7 +15,9 @@ function Board() {
   const [modalOn, setModalOn] = useState(false);
   const nextId = useRef(11);
 
-  // const [postList, setPostList] = useState(null);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * limit;
 
   const baseUrl = "http://localhost:8081";
 
@@ -58,7 +61,6 @@ function Board() {
       update_dt: item.update_dt,
     };
 
-    
     // console.log(selectedData);
     setSelected(selectedData);
   };
@@ -70,32 +72,94 @@ function Board() {
         <div className="text-xl font-bold mt-5 mb-3 text-center">
           <h1>게 시 판</h1>
         </div>
-        <Link to={{
-          pathname:"/post" ,
-          state: { isEdit: false }
-        }}>
-          <Button className="flex justify-center" color="lightBlue" buttonType="outline" size="regular" rounded={false}
-                  block={false} iconOnly={false} ripple="dark" >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+        <Link
+          to={{
+            pathname: "/post",
+            state: { isEdit: false },
+          }}
+        >
+          <Button
+            className="flex justify-center mb-8 text-xs px-3 py-2"
+            color="lightBlue"
+            buttonType="outline"
+            rounded={false}
+            block={false}
+            iconOnly={false}
+            ripple="dark"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 "
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4v16m8-8H4"
+              />
             </svg>
             글 쓰기
           </Button>
         </Link>
-        <table className="min-w-full table-auto text-gray-800">
-          <thead className="justify-between">
-            <tr className="bg-gray-800">
-              <th className="text-gray-300 px-4 py-3">번호</th>
-              <th className="text-gray-300 px-4 py-3">제목</th>
-              <th className="text-gray-300 px-4 py-3">작성자</th>
-              <th className="text-gray-300 px-4 py-3">최종 수정일</th>
-              <th className="text-gray-300 px-4 py-3">수정</th>
-              <th className="text-gray-300 px-4 py-3">삭제</th>
-            </tr>
-          </thead>
-          <Tr info={info} handleRemove={handleRemove} handleEdit={handleEdit} />
-        </table>
+
+        {/* <label>
+          페이지 당 표시할 게시물 수:&nbsp;
+          <select
+            type="number"
+            value={limit}
+            onChange={({ target: { value } }) => setLimit(Number(value))}
+          >
+            <option value="10">10</option>
+            <option value="12">12</option>
+            <option value="20">20</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+        </label> */}
+
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table class="w-full text-sm text-left text-gray-700 dark:text-gray-400">
+            <thead class="text-xs  text-gray-700  bg-gray-6000 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="text-gray-300 px-6 py-3">
+                  번호
+                </th>
+                <th scope="col" className="text-gray-300 px-6 py-3">
+                  제목
+                </th>
+                <th scope="col" className="text-gray-300 px-6 py-3">
+                  작성자
+                </th>
+                <th scope="col" className="text-gray-300 px-6 py-3">
+                  최종 수정일
+                </th>
+                <th scope="col" className="text-gray-300 px-6 py-3">
+                  수정
+                </th>
+                <th scope="col" className="text-gray-300 px-6 py-3">
+                  삭제
+                </th>
+              </tr>
+            </thead>
+            <Tr
+              info={info.slice(offset, offset + limit)}
+              handleRemove={handleRemove}
+              handleEdit={handleEdit}
+            />
+          </table>
+        </div>
       </div>
+      <footer>
+        <Pagination
+          total={info.length}
+          limit={limit}
+          page={page}
+          setPage={setPage}
+        />
+      </footer>
     </>
   );
 }
